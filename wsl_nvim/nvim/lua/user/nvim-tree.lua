5f76b428d097f090dc348124b199f4345e65ea3b
Algorant <Skantastico@gmail.com>
2023-07-03 18:54:10 -0400

    added treesitter

---
+++ wsl_nvim/nvim/lua/user/nvim-tree.lua
@@ -0,0 +1,21 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  on_attach = on_attach,
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
