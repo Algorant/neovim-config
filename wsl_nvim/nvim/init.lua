2b626c3fe6e3e693377e5fa8da10835e07c5c2b7
Algorant <Skantastico@gmail.com>
2023-07-03 18:56:03 -0400

    small refactor

--- wsl_nvim/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-
-
--- Setting Up My Dependencies and Files --
-require("user.keymaps")
-require("user.options")
-require("user.plugins")
-require("user.lualine")
-require("user.nvim-tree")
-require("user.treesitter")
